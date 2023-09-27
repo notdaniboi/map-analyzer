1175c48a0306dcb94ee95b2d199a932e0d0460a3
notdaniboi <144819170+notdaniboi@users.noreply.github.com>
2023-09-27 09:59:02 -0600

    Add files via upload

---
+++ solutions.js
@@ -0,0 +1,108 @@
+// SOLUTIONS TO MAP ANALYZER LEVELS
+
+// Global Variables
+let row, column; // Row & Column of Mouse on the Map
+let outputEl = document.getElementById("currentTile");
+
+// Solution Functions...
+function level1Solution() {
+  // USE BINARY IF STATEMENT
+  if (column <= 4) {
+    outputEl.innerHTML = "Sand";
+  } else {
+    outputEl.innerHTML = "Water";
+  }
+}
+
+function level2Solution() {
+  // USE CHAINED IF STATEMENT
+  if (column <= 2) {
+    outputEl.innerHTML = "hills";
+  } else if (column <= 7) {
+    outputEl.innerHTML = "sand";
+  } else {
+    outputEl.innerHTML = "water";
+  }
+}
+
+function level3Solution() {
+  // USE BINARY IF STATEMENT
+  if (column <= 7 && column >= 3) {
+    outputEl.innerHTML = "sand";
+  } else {
+    outputEl.innerHTML = "hills";
+  }
+}
+
+function level4Solution() {
+  // USE BINARY IF STATEMENT
+  if (row <= 2) {
+    outputEl.innerHTML = "hills";
+  } else {
+    outputEl.innerHTML = "sand";
+  }
+}
+
+function level5Solution() {
+  // USE CHAINED IF STATEMENT
+  if (row <= 2) {
+    outputEl.innerHTML = "hills";
+  } else if (row <= 5) {
+    outputEl.innerHTML = "sand";
+  } else {
+    outputEl.innerHTML = "water";
+  }
+}
+
+function level6Solution() {
+  // USE BINARY IF STATEMENT
+  if (row >= 2 && row <= 5) {
+    outputEl.innerHTML = "sand";
+  } else {
+    outputEl.innerHTML = "water";
+  }
+}
+
+function level7Solution() {
+  // USE BINARY IF STATEMENT
+  if (row 4 || column 7) {
+    outputEl.innerHTML = "water"
+  } else {
+    outputEl.innerHTML = "sand"
+  }
+}
+
+function level8Solution() {
+  // USE BINARY IF STATEMENT
+  outputEl.innerHTML = "----";
+}
+
+function level9Solution() {
+  // USE BINARY IF STATEMENT
+  outputEl.innerHTML = "----";
+}
+
+function level10Solution() {
+  // USE CHAINED IF STATEMENT
+  outputEl.innerHTML = "----";
+}
+
+function level11Solution() {
+  // USE CHAINED IF STATEMENT
+  outputEl.innerHTML = "----";
+}
+
+function level12Solution() {
+  // USE BINARY IF STATEMENT
+  outputEl.innerHTML = "----";
+}
+
+function level13Solution() {
+  // USE BINARY IF STATEMENT
+  outputEl.innerHTML = "----";
+}
+
+function level14Solution() {
+  // USE CHAINED IF STATEMENT
+  outputEl.innerHTML = "----";
+}
